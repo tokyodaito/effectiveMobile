f6dec41ea918987e78dc872ee1c4aa7bdaa677da
Bogdan <bogdanbogotov@gmail.com>
2024-01-31 15:10:48 +0300

    git init

---
+++ app/src/main/java/com/bogsnebes/effectivemobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bogsnebes.effectivemobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
